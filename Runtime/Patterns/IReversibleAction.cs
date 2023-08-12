254dec5b685f65cbdcc86fb1818529b8c3de0fa2
Kalen Arendt <dev.KalenArendt@Gmail.com>
2023-08-11 20:47:09 -0500

    added IReversibleAction

--- Runtime/Patterns/IReversibleAction.cs
+++ Runtime/Patterns/IReversibleAction.cs
@@ -1,8 +1,64 @@
-﻿namespace My.Core.Patterns
+﻿using System.Collections.Generic;
+
+namespace My.Core.Patterns
 {
    public interface IReversibleAction
    {
       void Redo ();
       void Undo ();
+   }
+
+   public class ActionManagerSingletonFactory : ISingletonFactory<ActionManager>
+   {
+      private static ActionManager instance;
+
+      public ActionManager GetSingleton => instance ??= new ActionManager();
+   }
+      
+   public class ActionManager
+   {
+      private readonly Stack<IReversibleAction> undoStack;
+      private readonly Stack<IReversibleAction> redoStack;
+
+      public ActionManager ()
+      {
+         undoStack = new Stack<IReversibleAction>();
+         redoStack = new Stack<IReversibleAction>();
+      }
+
+      // Add a new action to the undo stack
+      public void AddAction (IReversibleAction action)
+      {
+         undoStack.Push(action);
+         redoStack.Clear();
+      }
+
+      // Perform an undo operation
+      public void Undo ()
+      {
+         if (undoStack.Count > 0)
+         {
+            IReversibleAction action = undoStack.Pop();
+            action.Undo();
+            redoStack.Push(action);
+         }
+      }
+
+      // Perform an redo operation
+      public void Redo ()
+      {
+         if (redoStack.Count > 0)
+         {
+            IReversibleAction action = redoStack.Pop();
+            action.Redo();
+            undoStack.Push(action);
+         }
+      }
+
+      // Clear all actions from the stack
+      public void ClearActions ()
+      {
+         undoStack.Clear();
+      }
    }
 }
